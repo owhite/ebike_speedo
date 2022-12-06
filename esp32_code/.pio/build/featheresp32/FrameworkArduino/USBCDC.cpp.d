f74c44f6e28aadf0db56cb47ef04720d35f0530f
owhite <smouldering.dog@gmail.com>
2022-12-05 22:49:32 -0500

    stuff

---
+++ esp32_code/.pio/build/featheresp32/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/featheresp32/FrameworkArduino/USBCDC.cpp.o: \
+ /Users/owhite/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ /Users/owhite/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /Users/owhite/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
