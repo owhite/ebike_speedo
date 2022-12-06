140fda9fe474c1de4669ef6843128434cc369fb1
Owen White <smouldering.dog@gmail.com>
2022-12-06 14:05:26 -0500

    file cleanup

--- speedoLED/.pio/build/featheresp32/lib2b3/EWMA/Ewma.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/featheresp32/lib2b3/EWMA/Ewma.cpp.o: \
- .pio/libdeps/featheresp32/EWMA/Ewma.cpp \
- .pio/libdeps/featheresp32/EWMA/Ewma.h
--- speedoLED/.pio/build/featheresp32/lib37c/Adafruit GFX Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/featheresp32/lib37c/Adafruit\ GFX\ Library/glcdfont.c.o: \
- .pio/libdeps/featheresp32/Adafruit\ GFX\ Library/glcdfont.c
