ddd7a91fd2e61d5c556e9cba6d4dc9a7f7d3fd38
Owen White <smouldering.dog@gmail.com>
2023-05-01 16:11:42 -0400

    added neopixel code

--- ring_can_speedo/src/thing.ino
+++ ring_can_speedo/src/thing.ino
@@ -1,34 +1,29 @@
-#include <ST7735_t3.h>    // Hardware-specific library
-#include <ST7789_t3.h>    // Hardware-specific library
-#include <SPI.h>
+#include <Adafruit_NeoPixel.h>
 
-#define TFT_SCLK 13  // SCL
-#define TFT_MOSI 11  // SDA
-#define TFT_CS   10  // CS
-#define TFT_DC    9  // D/C
-#define TFT_RST   8  // RST can use any pin
+#define PIXEL_PIN 7 // Digital IO pin connected to the NeoPixels.
+#define PIXEL_COUNT 14 // Number of NeoPixels
 
-// Use one or the other
-ST7789_t3 tft = ST7789_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+int BRIGHTVAL = 0; // Variable to store birghness value in 
 
-int width = 160;
-int height = 128;
+Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup(void) {
-  Serial.begin(9600);
-  tft.initR(INITR_BLACKTAB); // if you're using a 1.8" TFT 128x160 displays
-  tft.setRotation(3);
+boolean oldState = HIGH;
+int mode = 0; // Currently-active animation mode, 0-9
 
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor((width / 2) - 40, height / 2);
-  tft.print("enjoy yourself");
-  tft.drawRect(1, 1, width - 2,height - 2, ST77XX_YELLOW);
+void setup() {
+ strip.begin(); // Initialize NeoPixel strip object (REQUIRED)
+ strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  Serial.print(tft.width());
-  Serial.print(" :: ");
-  Serial.println(tft.height());
+  colorWipe(strip.Color(255,   0,   0), 50);    // Red
 }
 
-
+void colorWipe(uint32_t color, int wait) {
+ for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+ strip.setPixelColor(i, color); // Set pixel's color (in RAM)
+ strip.setBrightness(BRIGHTVAL); // Set brightness value between 0-255
+ strip.show(); // Update strip to match
+ delay(wait); // Pause for a moment
+ }
+}
